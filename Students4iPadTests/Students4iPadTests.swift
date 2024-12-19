deca46e44888b9613cbc98eb167bef9509e7d99c
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-12-19 12:11:02 +0100

    Connection to remote server added

---
+++ Students4iPadTests/Students4iPadTests.swift
@@ -0,0 +1,17 @@
+//
+//  Students4iPadTests.swift
+//  Students4iPadTests
+//
+//  Created by Ivo Vondrak on 17.12.2024.
+//
+
+import Testing
+@testable import Students4iPad
+
+struct Students4iPadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
